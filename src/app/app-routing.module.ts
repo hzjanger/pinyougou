debaccb5ad1111569b2bbee26bb2d5f2a10c5d5e
hzjanger <1305696249@qq.com>
2019-07-09 00:09:13 +0800

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'managerAdmin',
+    loadChildren: () => import('./layout/manager-admin/manager-admin.module')
+      .then(module => module.ManagerAdminModule)
+  },
+  { path: '', redirectTo: 'managerAdmin', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
